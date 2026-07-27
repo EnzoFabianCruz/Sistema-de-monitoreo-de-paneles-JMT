cb6c8854620b03ca55d02094be6efd1547e20e5a
Enzuky <jewelryaccess7@gmail.com>
2026-07-27 17:10:28 -0500

    configuracion para el .env

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-# from decouple import config
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # <- esta línea carga el .env hacia os.environ
 
 def config(key, default=None):
     return os.environ.get(key, default)
@@ -145,7 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # solo si existe una carpeta global "static"
 ]
